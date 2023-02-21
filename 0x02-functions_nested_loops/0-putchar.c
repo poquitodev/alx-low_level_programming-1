b0c04d68f207abbc9326cd141225d3d614b32db6
elmahdirami535 <elmahdi.benrami535@gmail.com>
2023-02-21 02:35:25 -0800

    let's see

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * write a program that prints
+ * _putchar
+ * followed by a new line
+*/
+
+int main()
+{
+	printf("_putchar\n");
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+write a code using betty style again
